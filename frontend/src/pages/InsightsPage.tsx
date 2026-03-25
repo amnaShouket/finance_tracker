f1e9328c4ff2ece9ca1a1f62abc448bead383346
Amna Shouket <amnsho19@gmail.com>
2026-03-25 12:03:49 +0000

    Add bar chart to insights page

--- frontend/src/pages/InsightsPage.tsx
+++ frontend/src/pages/InsightsPage.tsx
@@ -1,9 +1,27 @@
 import { useFinance } from "../hooks/FinanceContext";   
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    Tooltip,
+    ResponsiveContainer,
+    CartesianGrid,
+} from "recharts";
 
 export default function  InsightsPage(){
     const {income, expenses, savingsGoal} = useFinance();
     const balance = income - expenses;
     const savingsProgress = savingsGoal === 0 ? 0 : Math.min((balance / savingsGoal)*100, 100);
+    const chartData = [
+        {name: "Income", value: income},
+        {name: "Expenses", value: expenses},
+        {name: "Balance", value: balance},
+    ];
+    
+
+
+
     return(
         <div>
             <h2 style = {{marginBottom: "24px"}}>Insights</h2>
@@ -38,6 +56,8 @@ export default function  InsightsPage(){
                     </p>
                 </div>
 
+                
+
                 <div style={{
                     background: "white",
                     padding: "20px",
@@ -59,7 +79,7 @@ export default function  InsightsPage(){
                             borderRadius: "999px",
                         }}>
                         </div>
-                        <p style = {{marginBottom: 0, marginTop: "8px"}}>
+                        <p style = {{margin: "12px 0"}}>
                             You have reached <strong>{savingsProgress.toFixed(1)}%</strong> of your monthly savings goal. 
                         </p>
                     </div>
@@ -78,6 +98,35 @@ export default function  InsightsPage(){
                     <p style = {{margin:"4px 0"}}>
                         {balance >= savingsGoal ? "You are meeting your savings goal!" : "You are below your savings goal. Try reducing expenses or increasing income"}
                     </p>
+                </div>
+
+                <div style ={{
+                    background: "white",
+                    padding: "20px",
+                    borderRadius: "12px",
+                    boxShadow: "0 1px 4px rgba(0,0,0,0.08)",
+                    marginBottom: "24px",
+             
+                }}>
+                    <h3 style = {{marginBottom: "12px"}}>
+                        Income vs Expenses
+                    </h3>
+                    <div style = {{width:"100%", height: 300}}>
+                        <ResponsiveContainer>
+                            <BarChart data = {chartData}>
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey = "name"/>
+                                <YAxis/>
+                                <Tooltip cursor = {false}
+                                formatter={(value) => [`$${value}`, "Amount"]}
+                                labelFormatter={(label) => `Category: ${label}`}
+                                />
+                                <Bar dataKey = "value"
+                                activeBar = {{fill: "#3a3a3a"}}
+                                />
+                            </BarChart>
+                        </ResponsiveContainer>
+                    </div>
                 </div>
             </div>
         </div>
